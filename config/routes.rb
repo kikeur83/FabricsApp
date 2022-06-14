36f929ddfecb43f0df0a81e1265096fca4b384a4
Georgi Mihov <georgi.mihov.06@gmail.com>
2022-06-14 10:47:54 +0200

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "users", to: "users#show"
+
+   resources :creations, only: [:show, :index] do
+    resources :favoris, only: [:new, :create]
+  end
+
+  resources :favoris, only: [:destroy]
 end
