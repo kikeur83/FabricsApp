9f439504b14f870a3b24e7c834d1237f93fd3e66
cd <charlydubostbp@hotmail.fr>
2022-06-17 16:19:39 +0200

    modif footer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_102634) do
+ActiveRecord::Schema.define(version: 2022_06_17_130117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_06_16_102634) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "time"
+    t.string "notice_pdf"
   end
 
   create_table "favoris", force: :cascade do |t|
