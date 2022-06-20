4528d16ed477fb79bcf0141c306ac38816c2af4b
cd <charlydubostbp@hotmail.fr>
2022-06-20 17:30:19 +0200

    font show manque caroussel

--- app/javascript/components/profile.js
+++ app/javascript/components/profile.js
@@ -1,19 +1,19 @@
 const profile = () => {
-   const buttons = document.querySelectorAll('.btnlinks')
+  const buttons = document.querySelectorAll('.btnlinks')
 
-   buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        const pages = document.querySelectorAll('.user-page-modal')
-        pages.forEach(page => {
-            page.classList.add('d-none');
-        })
-        const target = document.querySelector(`.${e.currentTarget.innerText.toLowerCase()}`)
-        target.classList.remove('d-none');
-        })
-
-   })
-
+  if (buttons) {
+    buttons.forEach(button => {
+      button.addEventListener('click', (e) => {
+          e.preventDefault();
+          const pages = document.querySelectorAll('.user-page-modal')
+          pages.forEach(page => {
+              page.classList.add('d-none');
+          })
+          const target = document.querySelector(`.${e.currentTarget.innerText.toLowerCase()}`)
+          target.classList.remove('d-none');
+          })
+    })
+  }
 }
 
 export{profile}
