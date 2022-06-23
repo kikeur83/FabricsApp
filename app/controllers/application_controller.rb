52bdec2f28a0ab5699331db0d6bd3aad09d069bc
Georgi Mihov <georgi.mihov.06@gmail.com>
2022-06-23 14:47:38 +0200

    meta done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def default_url_options
+    { host: ENV["www.fabricsapp.store"] || "localhost:3000" }
+  end
+
   protected
 
   def configure_permitted_parameters
